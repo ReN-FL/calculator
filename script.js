3e7569ec830ed93598c3db61a4995b5e33798a22
ReN <132862627+ReNlapf10@users.noreply.github.com>
2024-03-05 15:26:07 -0300

    Add multiplication and division functions

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function subtractNum(a, b) {
   let result = a - b;
   return result;
 }
+function multiplyNum(a, b) {
+  let result = a * b;
+  return result;
+}
+function divideNum(a, b) {
+  let result = a / b;
+  return result;
+}
