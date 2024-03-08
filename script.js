e2e86cd5edbad1160401dc6865ff8159dacd6e30
ReN <132862627+ReNlapf10@users.noreply.github.com>
2024-03-08 16:16:38 -0300

    Add UI controls

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 const operation = {
-  numA: 0,
+  numA: '',
   operator: '',
-  numB: 0,
+  numB: '',
 };
+const display = document.querySelector('#result-display');
+let numASet = false;
+let operatorSet = false;
+let decimalASet = false;
+let decimalBSet = false;
 function operate(a, z, b) {
-  let result;
-  switch (z) {
-    case '+':
-      result = addNum(a, b);
-      break;
-    case '-':
-      result = subtractNum(a, b);
-      break;
-    case 'x':
-      result = multiplyNum(a, b);
-      break;
-    case '/':
-      result = divideNum(a, b);
-      break;
+  if (a == 0 && z == '/' && b == 0) {
+    return ';-;';
+  } else {
+    let result;
+    switch (z) {
+      case '+':
+        result = addNum(a, b);
+        break;
+      case '-':
+        result = subtractNum(a, b);
+        break;
+      case 'x':
+        result = multiplyNum(a, b);
+        break;
+      case '/':
+        result = divideNum(a, b);
+        break;
+    }
+    result = Math.round(result * 100) / 100;
+    return result;
   }
-  return result;
 }
 function addNum(a, b) {
+  a = parseInt(a);
+  b = parseInt(b);
   let result = a + b;
   return result;
 }
@@ -36,4 +48,74 @@ function multiplyNum(a, b) {
 function divideNum(a, b) {
   let result = a / b;
   return result;
+}
+const inputNum = document.querySelectorAll('.button');
+inputNum.forEach((el) => {
+  el.addEventListener('click', controlUi);
+});
+function controlUi(e) {
+  if (!isNaN(e.target.value)) {
+    if (numASet) {
+      let num = e.target.value;
+      operation.numB += num;
+      display.textContent = operation.numB;
+    } else {
+      let num = e.target.value;
+      operation.numA += num;
+      display.textContent = operation.numA;
+    }
+  } else if (e.target.value === '=') {
+    if (!operatorSet) {
+      display.textContent = 'Please select an operator first';
+    } else {
+      let result = operate(operation.numA, operation.operator, operation.numB);
+      operation.numA = result;
+      operation.numB = '';
+      numASet = true;
+      display.textContent = result;
+      operatorSet = false;
+    }
+  } else if (e.target.value === 'clear') {
+    operation.numA = '';
+    operation.numB = '';
+    operation.operator = '';
+    operatorSet = false;
+    display.textContent = '0';
+    numASet = false;
+    decimalASet = false;
+    decimalBSet = false;
+  } else if (e.target.id == 'operator') {
+    if (operatorSet) {
+      let result = operate(operation.numA, operation.operator, operation.numB);
+      operation.numA = result;
+      operation.operator = e.target.value;
+      operation.numB = '';
+      display.textContent = `${result} ${e.target.value}`;
+      numASet = true;
+      operatorSet = true;
+    } else {
+      operation.operator = e.target.value;
+      operatorSet = true;
+      numASet = true;
+      display.textContent = e.target.value;
+    }
+  } else if (e.target.value == '.') {
+    if (numASet) {
+      if (decimalBSet) {
+      } else {
+        let decimal = e.target.value;
+        operation.numB += decimal;
+        display.textContent = operation.numB;
+        decimalBSet = true;
+      }
+    } else {
+      if (decimalASet) {
+      } else {
+        let decimal = e.target.value;
+        operation.numA += decimal;
+        display.textContent = operation.numA;
+        decimalASet = true;
+      }
+    }
+  }
 }
